d706767c7b2d5fa31fc788dae14beea1f0ff662b
haroon <haroon@bu.edu>
2025-10-08 05:58:49 -0400

    fixed urdf file and patched movit docker file to fix dependency issue causing setup assistant crash

---
+++ movit_ws/install/panda_description/share/panda_description/cmake/panda_descriptionConfig-version.cmake
@@ -0,0 +1,1 @@
+/movit_ws/build/panda_description/ament_cmake_core/panda_descriptionConfig-version.cmake
---
+++ movit_ws/install/panda_description/share/panda_description/local_setup.sh
@@ -0,0 +1,1 @@
+/movit_ws/build/panda_description/ament_cmake_environment_hooks/local_setup.sh
